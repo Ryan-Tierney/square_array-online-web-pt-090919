f7b05172b190adf6516c2cc3a8c62bffe47da270
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-12 15:41:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,14 @@
 def square_array(array)
-  squared_numbers = []
-  array.each do |numbers|
-    squared_numbers << numbers ** 2 
-  end
-  return squared_numbers
+  numbers = [1, 2, 3]
+  numbers.each do |number| 
+    numbers << number ** 2 
+  end 
+end
+
+
+
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
